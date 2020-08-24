94f602452337fb8e0ca239c63fd1b5878a8ab80e
David Barbour <danceguy303@gmail.com>
2020-08-24 05:50:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && position_taken?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
     return true
   end
 end
